571a89b31e02b22d5970ccaa56053f5fb1ea764a
Nikhil Tirumalasetty <tirumalasettynikhil@gmail.com>
2026-02-01 20:32:27 +0530

    Reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
 import { Header } from "@/components/header";
 import { HeroSection } from "@/components/hero-section";
 import { Trust } from "@/components/trust";
-import { FeaturesSection } from "@/components/features";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center selection:bg-brand-200 bg-white font-san">
       <Header />
-      <main className="flex min-h-screen w-full flex-col py-14 sm:py-14 lg:py-20 mt-8 sm:mt-16 lg:mt-16 bg-white gap-20">
+      <main className="flex min-h-screen w-full flex-col py-14 sm:py-14 lg:py-20 mt-8 sm:mt-16 lg:mt-16 bg-white">
         <HeroSection />
         <Trust />
-        <FeaturesSection />
       </main>
     </div>
   );
