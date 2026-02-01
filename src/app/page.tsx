6c09beeaeb01e8c3f1717c1eec4c69ad2dbfeafc
Nikhil Tirumalasetty <tirumalasettynikhil@gmail.com>
2026-02-01 14:24:32 +0530

    Main tag styling changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center selection:bg-brand-200 bg-white font-san">
       <Header />
-      <main className="flex min-h-screen w-full flex-col py-26 mt-16 bg-white">
+      <main className="flex min-h-screen w-full flex-col py-14 sm:py-14 lg:py-26 mt-8 sm:mt-16 lg:mt-16 bg-white">
         <HeroSection />
       </main>
     </div>
